4cb62cce0edbf6fd23ee9e2d026a158fc581c932
muzaffarmhm <muzaffarmhm@gmail.com>
2021-12-04 23:05:27 +0530

    Views setup and webpage

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const session = require('express-session')
 const path = require('path')
-const { urlencoded } = require('body-parser')
 
 //Middleware
 
 app.set('view engine','ejs')
-app.set('views', path.join(__dirname + 'views' ))
+app.set('views', path.join(__dirname,  'views' ))
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000,()=>{
     console.log('Listening at 3000');
@@ -18,4 +18,8 @@ app.listen(3000,()=>{
 
 app.get('/',(req,res)=>{
     res.render('index')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('register')
 })
