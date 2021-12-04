57fcba867fbe96a09f432b6ae2128cd3590adebe
muzaffarmhm <muzaffarmhm@gmail.com>
2021-12-05 00:03:11 +0530

    Adding Register page

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ const path = require('path')
 const User = require('./models/users')
 
 mongoose.connect('mongodb://localhost:27017/auth')
-.then(()=> console.log('Mongo DB connected successfully..')
-.catch((err)=>console.log(err));
-
-
-
+    .then(()=> console.log('Mongo DB connected successfully..'))
+    .catch((err)=>console.log(err))
 
 //Middleware
 
@@ -19,6 +16,12 @@ app.set('view engine','ejs')
 app.set('views', path.join(__dirname,  'views' ))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+}))
+
 
 app.listen(3000,()=>{
     console.log('Listening at 3000');
@@ -30,4 +33,18 @@ app.get('/',(req,res)=>{
 
 app.get('/register',(req,res)=>{
     res.render('register')
+})
+
+app.post('/register',(req,res)=>{
+    
+    const {password} = req.body
+    bcrypt.hash(password,12, function(err, hash){
+        const user = new User(
+            req.body
+        )
+        user.password = hash
+        console.log(user)
+        user.save()
+    })
+    res.send('Login Page')
 })
